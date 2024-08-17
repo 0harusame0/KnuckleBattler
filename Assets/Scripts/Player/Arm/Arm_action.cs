59c7cf0c814d616cec692ab37cb1ad97d006f97d
0harusame0 <hiru0324@gmail.com>
2024-08-17 16:21:49 +0900

    test二回目
    なんか停電の影響で元々のbranchがアンノーンになっちゃったんでクローンしなおしてテスト

--- Assets/Scripts/Player/Arm/Arm_action.cs
+++ Assets/Scripts/Player/Arm/Arm_action.cs
@@ -14,21 +14,23 @@ public class Arm_action : MonoBehaviour
     [SerializeField] Arm_flring flring;
 
     private Rigidbody rb;
-    private Rigidbody Prb;
+    private Rigidbody PCc;
     private Transform tf;
     private Vector3 camerapoint;
     private Vector3 handpoint;
     private Vector3 handmokuhyoupoint;
     private Vector3 ShotAngle;
+    private Vector3 point;
 
-    private bool Shotf = false;
+    private bool Shotf = false;//òrÇ™îÚÇ‘Ç∆Ç´Ç…ON
+    private bool Ankar = false;//òrÇ™ínñ ìôÇ…ê⁄íÖÇµÇƒÇÈéûÇ…ON
     private float timecount = 0.0f;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        Prb = GetComponent<Rigidbody>();
+        PCc = player.GetComponent<Rigidbody>();
         tf = GetComponent<Transform>();
     }
 
@@ -40,7 +42,7 @@ public class Arm_action : MonoBehaviour
             camerapoint = kamera.transform.position;
             handpoint = hand.transform.position;
 
-            Vector3 point;
+            
             if (Shotf)
             {
                point = Arm_Trshot();
@@ -50,8 +52,16 @@ public class Arm_action : MonoBehaviour
                point = Arm_move();
             }
             Vector3 angle = Arm_angles();
-            rb.velocity = point;
+
+            rb.velocity =point;
             tf.localEulerAngles = angle;
+            
+            if (false)
+            {
+                PCc.AddForce(point * -1);
+            }
+            
+            
         }
     }
 
@@ -60,8 +70,8 @@ public class Arm_action : MonoBehaviour
             camerapoint.y = camerapoint.y + hedoffset;
             Vector3 c = handpoint - camerapoint;
             c = c.normalized;
-            Vector3 point = handpoint + c * kyori;
-            handmokuhyoupoint = (point - tf.position) * 10;
+            Vector3 nextpoint = handpoint + c * kyori;         
+            handmokuhyoupoint = (nextpoint - tf.position) * 10;
             return handmokuhyoupoint;
     }
 
@@ -69,7 +79,9 @@ public class Arm_action : MonoBehaviour
     {
            Vector3 hedkaiten = player.transform.localEulerAngles - kamera.transform.localEulerAngles;
            Vector3 handangle = hand.transform.localEulerAngles + new Vector3(0.0f, hedkaiten.y, 0.0f);
-           return handangle;
+        //return handangle;
+
+        return hand.transform.eulerAngles;
     }
 
     private Vector3 Arm_Trshot()
@@ -87,5 +99,21 @@ public class Arm_action : MonoBehaviour
             ShotAngle = tf.forward;
             timecount = 0.0f;
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        var Tag = collision.gameObject.tag;
+        if(Tag != "Player")
+        {
+            Ankar = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Ankar = false;
+    }
+    public Vector3 Handekutoru()
+    {
+        return point;
     }
 }
--- Assets/Scripts/Player/Grab/Grab_suffer.cs
+++ Assets/Scripts/Player/Grab/Grab_suffer.cs
@@ -4,44 +4,151 @@ using UnityEngine;
 
 public class Grab_suffer : MonoBehaviour
 {
-    private bool Grab_Trigger = false;
-    private GameObject hand;
-    private Transform handTf;
-    private Rigidbody myRB;
-    private Transform myTf;
-    private Vector3 speed=Vector3.zero;
+    [Tooltip("掴まれて振り回された時の攻撃力")]
+    [SerializeField] int GrabAttackPower = 0;
+    [SerializeField] GameObject tuibisaki;
+    private float AttackSpeedLimit = 1f;
+    private float AttackThrownSpeedLimit = 2f;
+
+    private bool Grab_Trigger = false;       //捕まれているかの判断
+    private bool Atack_Trigger = false; //攻撃判定が発生しているかの判断
+    private bool thrown_Tracking = false; //追尾判定が発生しているかの判断
+    private bool Grab_thrown = false;        //投げられているかの判断。ここがfalseになってようやく一連の流れが終了する
+    private float thrwn_speed = 0.0f;
+    private GameObject hand;//手を取得
+    private GameObject taget_ob;//追尾するオブジェクト
+    private Transform handTf;//手のトランスフォームを取得
+    private Rigidbody handRB;//手のリジットボディを取得
+    private Grab_hand handcs;//手のGrab_handへのアクセスを取得
+    private Rigidbody myRB;//自分のリジットボディを取得
+    private Transform myTf;//自分のトランスフォームを取得
+    private Vector3 speed=Vector3.zero;//自身のスピードを取得
     public void GrabStart(GameObject Grab_hand)//捕まれた際に最初に行う事
     {
         
         hand = Grab_hand;
         handTf = hand.GetComponent<Transform>();
+        handRB = hand.GetComponent<Rigidbody>();
+        handcs = hand.GetComponent<Grab_hand>();
         Grab_Trigger = true;
-
+        
     }
-    public void GrabEnd()//離された際に行う事
+    public void GrabEnd(Vector3 bekutoru)//離された際に行う事
     {
         Grab_Trigger = false;
-        myRB.AddForce(speed, ForceMode.Impulse);
-        speed = Vector3.zero;
+        Grab_thrown = true;
         hand = null;
         handTf = null;
+        myRB.velocity = bekutoru;
+        thrwn_speed = myRB.velocity.magnitude;
+        if (taget_ob != null)
+        {
+            thrown_Tracking = true;
+        }
+        else
+        {
+            thrown_Tracking = false;
+        }
+        
     }
 
+    public void GrabEndTaget(GameObject enemy)//追尾対象があった際にここにオブジェクトを出します
+    {
+        taget_ob = enemy;
+    }
     public bool GrabCeak()//同オブジェクトの他のスクリプトから持たれているかチェックするよ
     {
         return Grab_Trigger;
+    }
+    public bool GrabAtackCeak()//他のスクリプトからアタック判定になっているかチェックするよ
+    {
+        return Atack_Trigger;
+    }
+    public bool ThrownCeak()//他のスクリプトから投げ飛ばされているかチェックするよ
+    {
+        return Grab_thrown;
     }
 
     private void Start()
     {
         myRB = this.gameObject.GetComponent<Rigidbody>();
         myTf = this.gameObject.GetComponent<Transform>();
+        if(tuibisaki != null)
+        {
+            taget_ob = tuibisaki;
+        }
     }
     private void FixedUpdate()
     {
         if (Grab_Trigger)
         {
-            speed = myRB.velocity;
+            if(myRB.velocity.magnitude >= AttackSpeedLimit)
+            {
+                Atack_Trigger = true;
+            }
+            else
+            {
+                Atack_Trigger = false;
+            }
+        }
+        if (thrown_Tracking)//追尾モードがオンになっていたら追尾する
+        {
+            Vector3 mokutekiti = taget_ob.transform.position - myTf.position;
+            mokutekiti = mokutekiti.normalized * thrwn_speed;
+            myRB.velocity = mokutekiti;
+            if (!Atack_Trigger)
+            {
+                Atack_Trigger = true;
+            }
+        }
+        if (Grab_thrown)
+        {
+            
+            if(myRB.velocity.magnitude <= AttackThrownSpeedLimit)
+            {
+                Grab_thrown = false;
+                thrown_Tracking = false;
+                Atack_Trigger = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (Grab_Trigger)
+        {
+            
+            if( Atack_Trigger && collision.gameObject.tag != "Player")
+            {
+                Sutetasu status = collision.gameObject.GetComponent<Sutetasu>();
+                
+                if ( status != null)
+                {
+                    status.Damage(GrabAttackPower);
+                    
+                }
+            }
+
+        }
+        else if (Grab_thrown)
+        {
+            if(myRB.velocity.magnitude >= AttackThrownSpeedLimit && collision.gameObject.tag != "Player")
+            {
+                Sutetasu status = collision.gameObject.GetComponent<Sutetasu>();
+                
+                if (status != null)
+                {
+                    status.Damage(GrabAttackPower);
+                }
+            }
+            
+            if (thrown_Tracking)
+            {
+                thrown_Tracking = false;
+                //taget_ob = null;
+            }
+
         }
     }
 
